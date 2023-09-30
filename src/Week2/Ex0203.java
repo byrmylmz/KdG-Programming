18c95d50fda20cf35b6b441cec7d72cf4ad7022b
byrmylmz <bayramyilmaz061@gmail.com>
2023-09-30 18:11:02 +0200

    ex0203

--- src/Week2/Ex0203.java
+++ src/Week2/Ex0203.java
@@ -1,12 +1,41 @@
 package Week2;
 
 
+import java.util.Scanner;
+
 public class Ex0203 {
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
+        double vat;
+        double amount;
+        int choice;
 
-        // Part 1
+        System.out.println("Enter the VAT percentage");
 
+        Scanner keyboard = new Scanner(System.in);
+
+        vat = keyboard.nextDouble();
+
+        System.out.println("Enter an amount in €:");
+
+        amount = keyboard.nextDouble();
+
+        System.out.println("Make a choice (1 = inclusive, 2 = exclusive):");
+
+        choice = keyboard.nextInt();
+
+        if (choice == 1) {
+            System.out.println("€" + (amount / (vat / 100 + 1)) + " + " + vat + "% VAT = €" + amount);
+
+        } else if (choice == 2) {
+            System.out.println("€" + amount + " + " + vat + "% VAT = €" + (amount + (amount * vat / 100)));
+
+        } else {
+            System.out.println("Invalid Choice!");
+
+        }
+
+        keyboard.close();
 
     }
 }
